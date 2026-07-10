800ab248913bbea718ba2b2e2d32824720b8727c
Sairam Ganisetti <sairam.ganisetti@1998.com>
2026-07-10 16:46:41 +0530

    commit

---
+++ Playwright_TestProject/PlayWrightWithNUnit/LoginToORMWithPlayWright.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Playwright_TestProject.PlayWrightWithNUnit
+{
+    public class LoginToORMWithPlayWright :PageTest
+    {
+        [Test]
+        public async Task LoginHrm()
+        {
+            await Page.GotoAsync("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+            //ILocator username = Page.GetByRole(AriaRole.Textbox, new() { Name = "username" });
+            // ILocator password = Page.GetByRole(AriaRole.Textbox, new() { Name = "password" });
+            // ILocator login_btn = Page.GetByRole(AriaRole.Button, new() { Name = " submit"});
+            ILocator username = Page.GetByPlaceholder("Username");
+            ILocator password = Page.GetByPlaceholder("Password");
+            ILocator login_btn = Page.GetByRole(AriaRole.Button, new() { Name = "Login" });
+            await username.FillAsync("admin");
+            await password.FillAsync("admin123");
+            await login_btn.ClickAsync();
+        }
+    }
+}
