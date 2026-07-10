67a38b661d461387de084508b74c77d43eefafd5
Sairam Ganisetti <sairam.ganisetti@1998.com>
2026-07-10 12:22:46 +0530

    firstTc

---
+++ Playwright_TestProject/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Playwright;
+
+namespace Playwright_TestProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public async Task Test1()
+        {
+            // Playwright
+            var playwright = await Playwright.CreateAsync();
+            //Browser
+            await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = false
+            });
+            //page
+
+            var page = await browser.NewPageAsync();
+            await page.GotoAsync("http://eaapp.somee.com/");
+            await page.ClickAsync("text = Login");
+            await page.ScreenshotAsync(new PageScreenshotOptions {
+                
+                Path =  "Eaapp.jpg"
+            
+            });
+// first Tc
+        }
+    }
+}
